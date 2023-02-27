8d604222a0db229f96715aa3f3c7d4f590829a3b
Karl9Doniz <andrei.agitolyev@gmail.com>
2023-02-27 21:35:40 +0200

    updated main python file, added html templates

--- markets_map_app.py
+++ markets_map_app.py
@@ -9,33 +9,43 @@ https://github.com/Karl9Doniz/spotify_api
 
 
 import json
-from dotenv import load_dotenv
-import os
 import base64
 import folium
 from requests import post, get
 from geopy.geocoders import Nominatim
 import pycountry
+from flask import Flask, render_template, request
 
-from flask import Flask, render_template
-
+# Flask app
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("markets_locations.html")
+@app.route("/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        token = get_token()
+        name = request.form["nm"]
+        result = search_for_artist(token, name)
+        artist_id = result["id"]
+        top_songs = get_info_by_artist(token, artist_id)
 
-load_dotenv()
+        song = top_songs[0]
+        id_song = song['id']
+        countries = get_info_by_track(token, id_song)
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+        map_plot(countries)
+        return render_template("markets_locations.html")
+    else:
+        return render_template("login.html")
+
+CLIENT_ID="8aa6712d52d94dd68cec77e607aa9d89"
+CLIENT_SECRET="dcc2d11a3c924a46b4e3bd58930891f9"
 
 def get_token():
     """
     Gets token for further work with API.
     """
 
-    auth_string = client_id + ":" + client_secret
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
@@ -103,12 +113,12 @@ def get_info_by_track(token, song_id):
 
 
 def map_plot(countries):
-    map = folium.Map(tiles="Stamen Terrain")
+    map = folium.Map(tiles="Stamen Terrain", zoom_start=10)
     fg = folium.FeatureGroup(name="Markets map")
     geolocator = Nominatim(user_agent="http")
     coords = []
 
-    for market in countries[:15]:
+    for market in countries[:20]:
         market_name = pycountry.countries.get(alpha_2=market).name
         location = geolocator.geocode(market_name)
         coords.append((location.latitude, location.longitude, market_name))
@@ -116,24 +126,10 @@ def map_plot(countries):
     for coord in coords:
         fg.add_child(folium.Marker(location=[coord[0], coord[1]], popup=coord[2], 
                                    icon=folium.Icon()))
-    
-    map.add_child(fg)
-    map.save("/Users/admin/ucu_programming/spotify/spotify_api/templates/markets_locations.html")
 
+    map.add_child(fg)
+    map.save("spotify_api/templates/markets_locations.html")
 
 
 if __name__ == "__main__":
-    token = get_token()
-    name = input()
-    result = search_for_artist(token, name)
-    artist_id = result["id"]
-    top_songs = get_info_by_artist(token, artist_id)
-
-    print("Artist: " + name + f" (id:{artist_id})")
-
-    song = top_songs[0]
-    id_song = song['id']
-    countries = get_info_by_track(token, id_song)
-
-    map_plot(countries)
     app.run()
