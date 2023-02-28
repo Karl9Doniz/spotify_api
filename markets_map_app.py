7a894fb1a6529c569391cc11bb799cd48e0ab317
Karl9Doniz <andrei.agitolyev@gmail.com>
2023-02-28 03:08:23 +0200

    updated main python file

--- markets_map_app.py
+++ markets_map_app.py
@@ -12,9 +12,7 @@ import json
 import base64
 import folium
 from requests import post, get
-from geopy.geocoders import Nominatim
-import pycountry
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 # Flask app
 app = Flask(__name__)
@@ -32,10 +30,14 @@ def login():
         id_song = song['id']
         countries = get_info_by_track(token, id_song)
 
-        map_plot(countries)
-        return render_template("markets_locations.html")
+        map_plot(countries, song['name'])
+        return redirect(url_for("markets", song=song['name']))
     else:
         return render_template("login.html")
+    
+@app.route("/<song>")
+def markets(song):
+    return render_template("markets_locations.html")
 
 CLIENT_ID="8aa6712d52d94dd68cec77e607aa9d89"
 CLIENT_SECRET="dcc2d11a3c924a46b4e3bd58930891f9"
@@ -90,9 +92,10 @@ def search_for_artist(token, artist_name):
 
 def get_info_by_artist(token, artist_id):
     """
-    Returns top 10 songs (and albums where they appear + year of release).
+    Returns top 10 songs.
     """
-
+    # countries = csv_to_dict.keys()
+    # for code in countries:
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
@@ -112,24 +115,40 @@ def get_info_by_track(token, song_id):
     return json_result
 
 
-def map_plot(countries):
-    map = folium.Map(tiles="Stamen Terrain", zoom_start=10)
+def map_plot(countries, name):
+    map = folium.Map(tiles="Stamen Terrain", zoom_start=20)
     fg = folium.FeatureGroup(name="Markets map")
-    geolocator = Nominatim(user_agent="http")
+    # geolocator = Nominatim(user_agent="http")
     coords = []
+    country_coord = csv_to_dict()
 
-    for market in countries[:20]:
-        market_name = pycountry.countries.get(alpha_2=market).name
-        location = geolocator.geocode(market_name)
-        coords.append((location.latitude, location.longitude, market_name))
+    for market in countries:
+        if market in country_coord.keys():
+            coords.append((country_coord[market][1], country_coord[market][2],
+                        country_coord[market][0]))
 
     for coord in coords:
         fg.add_child(folium.Marker(location=[coord[0], coord[1]], popup=coord[2], 
                                    icon=folium.Icon()))
 
+
+    title_html = f'''
+             <h3 align="center" style="font-size:30px"><b>Showing map for: "{name}"</b></h3>
+             '''
+    map.get_root().html.add_child(folium.Element(title_html))
     map.add_child(fg)
     map.save("spotify_api/templates/markets_locations.html")
 
 
+def csv_to_dict():
+    result = {}
+    with open('countries.csv', 'r') as file:
+        for line in file:
+            line = line.split(",")
+            result[line[0]] = (line[1], float(line[2]), float(line[3].strip()))
+    return result
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    
